c0698af9ad5a26ba9eb02eca748f9c7d9ab10c01
Ettienne63 <ettienne.jvv@icloud.com>
2026-03-05 10:35:40 +0200

    added contact page, aswell fiixed some style things

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use((req, res, next) => {
 
 app.get("/", (_req, res) => res.render("home"));
 app.get("/about", (_req, res) => res.render("about"));
+app.get("/contact", (_req, res) => res.render("contact"));
 app.get("/admin", requireAdmin, asyncHandler(productController.getAdminPage));
 app.get(
   "/admin/affiliate",
